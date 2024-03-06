88de4ebb56ecd8187d39d5e630a1d2fe4a50c3ae
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-03-06 22:24:31 +0100

    feat: adding Notes for setting up  JavaScript Structure

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("TEST");
+
+//Hinzufügen neuer Todos
+
+// Erledigt-Status von Todos
+
+// Duplikatprüfung
+
+//Lokaler Speicher Adaption
+
+//Filtern von Todos
+
+//Erledigte ToDos entfernen

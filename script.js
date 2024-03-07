88113ad7504ab30944ef8b5e04524d344045360e
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-03-07 11:17:23 +0100

    feat: adding function to add tasks to task ctn

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 console.log("TEST");
 
 //Hinzufügen neuer Todos
+const addButton = document.querySelector(".btn.add");
+const input = document.querySelector("#task-input");
+const taskContainer = document.querySelector(".ctn-tasks");
+
+function addInput(params) {
+  if (inputValue !== " ") {
+    //new task-ctn
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+
+    //new checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    //new span
+    const taskText = document.createElement("span");
+    taskText.textContent = inputValue;
+
+    //adding checkbox & span to taskItem div
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskText);
+
+    //adding to ctn-tasks
+    taskContainer.appendChild(taskItem);
+  }
+}
+
+addButton.addEventListener("click", addInput);
 
 // Erledigt-Status von Todos
 
